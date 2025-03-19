0e96720a3421b171a0073ddce27257d2cd933ba2
meeeiiike <g00407095@atu.ie>
2025-03-19 15:40:54 +0000

    added tests for success and failure for subtract and multiply TODO: divide and add extra possible exception types

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -26,4 +26,30 @@ public class CalcTest {
         );
         assertEquals("Value is Too Large", e.getMessage());
     }
+
+    @Test
+    void testSubtractSuccess(){
+        assertEquals(4, calc.subtract(8,4));
+    }
+    @Test
+    void testSubtractFailure(){
+
+        Exception e = assertThrows(IllegalArgumentException.class, ()-> {
+            calc.subtract(Integer.MIN_VALUE, 1);
+        });
+        assertEquals("Value is too Small", e.getMessage());
+    }
+
+    @Test
+    void testMultiplySuccess(){
+        assertEquals(4, calc.multiply(2,2));
+    }
+
+    @Test
+    void testMultiplyFailure(){
+        Exception e = assertThrows(IllegalArgumentException.class, ()-> {
+            calc.multiply(Integer.MAX_VALUE, 2);
+        });
+        assertEquals("Value is too Large", e.getMessage());
+    }
 }
